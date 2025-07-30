24449614d7e0c644d9d2c5a0dc4889b46bc6e3f8
BayramovZiya88 <bayramovziya88@gmail.com>
2025-07-30 20:38:24 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Firebase config (senin verdiklerinle)
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7WhkcW6JbBD6rYkm0O7JmRu5vt_tGzmY",
   authDomain: "droxicheats.firebaseapp.com",
@@ -8,39 +8,39 @@ const firebaseConfig = {
   appId: "1:1099305011676:web:9e58b3d7a02d7108dea719"
 };
 
-// Firebase başlat
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
-// Kayıt
+
 function register() {
   const email = document.getElementById('register-email').value;
   const password = document.getElementById('register-password').value;
 
   auth.createUserWithEmailAndPassword(email, password)
     .then(userCredential => {
-      alert('Kayıt başarılı: ' + userCredential.user.email);
+      window.location.href = "home.html";
     })
     .catch(error => {
       alert('Hata: ' + error.message);
     });
 }
 
-// Giriş
+
 function login() {
   const email = document.getElementById('login-email').value;
   const password = document.getElementById('login-password').value;
 
   auth.signInWithEmailAndPassword(email, password)
     .then(userCredential => {
-      alert('Giriş başarılı: ' + userCredential.user.email);
+      window.location.href = "home.html";
     })
     .catch(error => {
       alert('Hata: ' + error.message);
     });
 }
 
-// Google ile Giriş
+
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -53,7 +53,7 @@ function googleLogin() {
     });
 }
 
-// Oturum takibi (isteğe bağlı)
+
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log("Giriş yapan:", user.email);
